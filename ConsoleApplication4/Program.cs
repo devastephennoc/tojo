a78ad95b6b998837fa89806adc1a6bb6a50dd7ec
devastephennoc <devastephennoc@gmail.com>
2017-09-26 22:18:51 +0530

    Add project files.

---
+++ ConsoleApplication4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Synthesis;
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+
+            // Configure the audio output. 
+            synth.SetOutputToDefaultAudioDevice();
+            // Speak a string.
+            synth.Speak("This example demonstrates a basic use of Speech Synthesizer");
+            synth.Speak("Press any key to exit...");
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
